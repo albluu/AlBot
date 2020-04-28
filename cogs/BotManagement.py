232906ae583c2b655b662bb02ab61b0db7a75853
Albert Luu <albert.luu.251@gmail.com>
2020-04-28 13:27:27 -0600

    Set bot management commands to owner only

--- cogs/BotManagement.py
+++ cogs/BotManagement.py
@@ -6,16 +6,19 @@ class BotManagement(commands.Cog):
         self.bot = bot
 
     @commands.command(hidden = True)
+    @commands.is_owner()
     async def reloadext(self, ctx, ext: str):
         await ctx.send('Reloading extension {0}'.format(ext))
         self.bot.reload_extension(ext)
 
     @commands.command(hidden = True)
+    @commands.is_owner()
     async def startext(self, ctx, ext: str):
         await ctx.send('Starting extension {0}'.format(ext))
         self.bot.load_extension(ext)
 
     @commands.command(hidden = True)
+    @commands.is_owner()
     async def stopext(self, ctx, ext: str):
         await ctx.send('Stopping extension {0}'.format(ext))
         self.bot.unload_extension(ext)
