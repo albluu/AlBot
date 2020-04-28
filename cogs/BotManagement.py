3cfbe387eca98a65b92dfeb2f4cfcf81316450c7
Albert Luu <albert.luu.251@gmail.com>
2020-04-28 15:04:34 -0600

    Move to using checks instead of if cases

--- cogs/BotManagement.py
+++ cogs/BotManagement.py
@@ -6,19 +6,28 @@ class BotManagement(commands.Cog):
         self.bot = bot
 
     @commands.command(hidden = True)
+    @commands.is_owner()
     async def reloadext(self, ctx, ext: str):
         await ctx.send('Reloading extension {0}'.format(ext))
         self.bot.reload_extension(ext)
 
     @commands.command(hidden = True)
+    @commands.is_owner()
     async def startext(self, ctx, ext: str):
         await ctx.send('Starting extension {0}'.format(ext))
         self.bot.load_extension(ext)
 
     @commands.command(hidden = True)
+    @commands.is_owner()
     async def stopext(self, ctx, ext: str):
         await ctx.send('Stopping extension {0}'.format(ext))
         self.bot.unload_extension(ext)
+
+    @commands.command(hidden = True)
+    @commands.is_owner()
+    async def shutdownbot(self, ctx):
+        await ctx.send('Shutting down the bot.')
+        await self.bot.logout()
 
 def setup(bot: commands.Bot):
     bot.add_cog(BotManagement(bot))
